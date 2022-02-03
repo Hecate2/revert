4a3cb10fe3b1aa45fa526ad73867bd60f2a0e5af
Hecate2 <2474101468@qq.com>
2022-02-03 19:39:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "Checking if a PR command..."
 (jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
 
 # get the SHA to revert
-COMMIT_TO_REVERT=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | cut -c 9-)
+# COMMIT_TO_REVERT=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | cut -c 9-)
 
 if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
 	echo "This is not a new comment event!"
@@ -49,6 +49,14 @@ git fetch origin $HEAD_BRANCH
 git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
 # check commit exists
-git cat-file -t $COMMIT_TO_REVERT
-git revert $COMMIT_TO_REVERT --no-edit
+# git cat-file -t $COMMIT_TO_REVERT
+# git revert $COMMIT_TO_REVERT --no-edit
+
+git cat-file -t e542e2eb2e937520a637de71b84a30ab2986b901
+git revert e542e2eb2e937520a637de71b84a30ab2986b901 --no-edit
+git cat-file -t 7c91a20b4a019a8bab4869f6124eccdd4281cef2
+git revert 7c91a20b4a019a8bab4869f6124eccdd4281cef2 --no-edit
+git cat-file -t 0d97ed12a4d561a76ade885d79d4451a63dfba42
+git revert 0d97ed12a4d561a76ade885d79d4451a63dfba42 --no-edit
+
 git push origin $HEAD_BRANCH
